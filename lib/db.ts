30bf1e37bc530659f17028627bdb2559943b3082
mughisqarratal <cutqarratalmughis@gmail.com>
2026-02-17 15:29:58 +0700

    Perbaikan Regist

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,12 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gunners",
+  // Mencoba membaca dari server (Hostinger), jika tidak ada pakai 'localhost' (Laragon)
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "gunners",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
